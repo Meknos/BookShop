e11ddc6829d0e69cf859f68d4a25c33009521f87
Son Nguyen <nguyentranson2k3@gmail.com>
2023-11-25 16:37:21 +0700

    CRUD Book

--- BookShop/Controllers/CategoryController.cs
+++ BookShop/Controllers/CategoryController.cs
@@ -4,21 +4,91 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookShop.Controllers
 {
-    public class CategoryController : Controller
-    {
-        private readonly ApplicationDBContext _dbContext;
-        public CategoryController(ApplicationDBContext applicationDBContext)
-        {
-            _dbContext = applicationDBContext;
-        }
-        public IActionResult Index()
-        {
-            List<Category> categories = _dbContext.Categories.ToList();
-            return View(categories);
-        }
-        public IActionResult Detail()
-        {
-            return View();
-        }
-    }
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDBContext _dbContext;
+		public CategoryController(ApplicationDBContext applicationDBContext)
+		{
+			_dbContext = applicationDBContext;
+		}
+		public IActionResult Index()
+		{
+			List<Category> categories = _dbContext.Categories.ToList();
+			return View(categories);
+		}
+		public IActionResult Details()
+		{
+			return View();
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Category category)
+		{
+
+			if (category.Name == category.Description)
+			{
+				ModelState.AddModelError("Description", "Name can not be equal to Description");
+			}
+			if (ModelState.IsValid)
+			{
+				_dbContext.Categories.Add(category);
+				_dbContext.SaveChanges();
+				TempData["success"] = "Category Created successfully";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? category = _dbContext.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				_dbContext.Categories.Update(category);
+				_dbContext.SaveChanges();
+				TempData["success"] = "Category Updated successfully";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? category = _dbContext.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost]
+		public IActionResult Delete(Category category)
+		{
+				_dbContext.Categories.Remove(category);
+				_dbContext.SaveChanges();
+				TempData["success"] = "Category Delete successfully";
+				return RedirectToAction("Index");
+		}
+
+
+	}
 }
---
+++ BookShop/Migrations/20231125092343_addbook.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class addbook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "CategoryId", "Description", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Microsoft", 2, "Hello", 10.0, "C# Programming" },
+                    { 2, "BTEC", 2, "Learning Harder", 11.0, "Advanced Programming" },
+                    { 3, "Sun", 2, "Basic language", 15.0, "Java Programming" },
+                    { 4, "Greenwich", 2, "Really not easy", 20.0, "Data Structures" },
+                    { 5, "Microsoft", 2, "Now", 10.0, "App Dev" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_CategoryId",
+                table: "Books",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
